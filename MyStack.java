bcc39fdcb1c17f7719c22bd234c0bef8d19f175c
Joshua Zhang <jzhang2025@chadwickschool.org>
2023-09-20 11:35:52 -0700

    Almost Finished MyStack Class

--- MyStack.java
+++ MyStack.java
@@ -5,29 +5,81 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.io.*;
+import java.util.*;
 public class MyStack
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class MyStack
-     */
-    public MyStack()
+    private int[] stack;
+    private int size;
+   
+    public void MyStack(int length)
     {
-        // initialise instance variables
-        x = 0;
+       stack = new int[length];
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+
+    public void push(int element)
     {
-        // put your code here
-        return x + y;
+        stack[size] = element;
+        size++;
+    }
+    
+    public int pop()
+    {
+       if (isEmpty() == true){
+           return 0;
+       }
+       size--;
+       return stack[size];
+    }
+    
+    public boolean isEmpty()
+    {
+        if (size() == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public int top()
+    {
+        return stack[size-1];
+    }
+    
+    public int size()
+    {
+        return size;
+    }
+    
+    public boolean isFull()
+    {
+        if (size() >= stack.length){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public String toString()
+    {
+      int[] newArr = new int [size];
+      for (int i = 0; i < size; i++){
+          newArr[i] = stack[size - 1];
+      }
+      
+      return Arrays.toString(newArr);
+    }
+    
+    public void testCase()
+    { 
+        System.out.println(isEmpty()); 
+        System.out.println(size()); 
+        push(3); 
+        push(4);
+        System.out.println(size()); 
+        System.out.println(top()); 
+        System.out.println(pop()); 
+        System.out.println(stack.toString()); 
     }
 }
