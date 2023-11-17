e66180fb7469a42e514f1f254041897732a60a39
Joshua Zhang <jzhang2025@chadwickschool.org>
2023-11-17 11:28:24 -0800

    More comments

--- BinarySearchTreeNode.java
+++ BinarySearchTreeNode.java
@@ -16,6 +16,13 @@ public class BinarySearchTreeNode<E extends Comparable<E>>
         data = element;
     }
     
+    /**
+     * Inserts a new node into the tree and places it based off the integer 
+     * value. Checks if the element provided is less than or more than the root. 
+     *
+     * @param element The element to add.
+     */
+    
     public void insert(E element) {
     if (element.compareTo(data) < 0) {
         if (left == null) {
@@ -32,6 +39,12 @@ public class BinarySearchTreeNode<E extends Comparable<E>>
     }
     }
     
+    /**
+     * Checks if the element provided is in the tree and returns it. 
+     *
+     * @return The element if it is in the tree and null if the provided 
+     * element is not in the tree
+     */
     public E search(E element) {
     if (element.compareTo(data) == 0) {
         return data; 
@@ -43,6 +56,11 @@ public class BinarySearchTreeNode<E extends Comparable<E>>
     return null;    
     }
     
+    /**
+     * Searches for the smallest integer in the tree
+     *
+     * @return The smallest element in the tree 
+     */
     public E getMin() {
         if (left == null) { 
             return data;
@@ -51,6 +69,11 @@ public class BinarySearchTreeNode<E extends Comparable<E>>
         }
     }
 
+    /**
+     * Searches for the largest integer in the tree
+     *
+     * @return The largest element in the tree 
+     */
     public E getMax() {
         if (right == null) { 
             return data;
