a5a3efe7085305b3c5c1db7a90e01fbf5ddae02d
Joshua Zhang <jzhang2025@chadwickschool.org>
2023-11-26 21:36:41 -0800

    added the print tree method

--- BinarySearchTreeNode.java
+++ BinarySearchTreeNode.java
@@ -156,4 +156,94 @@ public class BinarySearchTreeNode<E extends Comparable<E>>
             }
         }
     }
+    
+    /**     * Prints elements as stored in the tree
+     *
+     * @param maxElementWidth The maximum space allowed for the string form
+     *                        of the element.
+     */
+    public void printTree(int maxElementWidth) {
+        int maxSize = (int) Math.pow(2, (getDepth() + 1));
+        String[] elements = new String[maxSize];
+
+        // Build array of elements
+        printTree(elements, 0, maxElementWidth);
+
+        // Print element properly spaced
+        int fullWidth = (int) Math.pow(2, getDepth()) * (maxElementWidth + 1);
+        for (int i = 0; i < getDepth() + 1; i++) {
+            String connectionsLevel = "";
+            String elementsLevel = "";
+
+            for (int j = (int) Math.pow(2, i) - 1; j < (int) Math.pow(2, i + 1) - 1; j++) {
+
+                // Process arrows for this level
+                String arrow = "  ";
+                int elementLength = arrow.length();
+                if (elements[j] != null) {
+                    if (j % 2 == 1) { // Odd is left child
+                        arrow = " /";
+                    } else { // Even is right child
+                        arrow = "\\ ";
+                    }
+                }
+
+                // Center within maxElementWidth
+                String leftPadStr = ""; // Default
+                String rightPadStr = ""; // Default
+                int leftPadNum = (fullWidth / (int) Math.pow(2, i) - elementLength) / 2;
+                int rightPadNum = fullWidth / (int) Math.pow(2, i) - elementLength - leftPadNum;
+                if (leftPadNum > 0) {
+                    leftPadStr = String.format("%" + leftPadNum + "s", " ");
+                }
+                if (rightPadNum > 0) {
+                    rightPadStr = String.format("%" + rightPadNum + "s", " ");
+                }
+                connectionsLevel += leftPadStr + arrow + rightPadStr;
+
+                // Process elements for this level
+                elementLength = 0;
+                if (elements[j] != null) {
+                    elementLength = elements[j].toString().length();
+                }
+
+                // Center within maxElementWidth
+                leftPadStr = ""; // Default
+                rightPadStr = ""; // Default
+                leftPadNum = (fullWidth / (int) Math.pow(2, i) - elementLength) / 2;
+                rightPadNum = fullWidth / (int) Math.pow(2, i) - elementLength - leftPadNum;
+                if (leftPadNum > 0) {
+                    leftPadStr = String.format("%" + leftPadNum + "s", " ");
+                }
+                if (rightPadNum > 0) {
+                    rightPadStr = String.format("%" + rightPadNum + "s", " ");
+                }
+
+                if (elements[j] != null) {
+                    elementsLevel += leftPadStr + elements[j] + rightPadStr;
+                } else {
+                    elementsLevel += leftPadStr + rightPadStr;
+                }
+            }
+
+            if (i > 0) { // Do not print arrows for root
+                System.out.println(connectionsLevel);
+            }
+            System.out.println(elementsLevel);
+        }
+    }
+
+    // build array of element strings
+    private void printTree(String[] elements, int nodeNum, int maxElementWidth) {
+        elements[nodeNum] = elements.toString();
+        if (elements[nodeNum].length() > maxElementWidth) {
+            elements[nodeNum] = elements[nodeNum].substring(0, maxElementWidth);
+        }
+        if (left != null) {
+            left.printTree(elements, (nodeNum + 1) * 2 - 1, maxElementWidth);
+        }
+        if (right != null) {
+            right.printTree(elements, (nodeNum + 1) * 2, maxElementWidth);
+        }
+    }
 }
